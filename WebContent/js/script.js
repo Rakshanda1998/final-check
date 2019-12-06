35e4be8aa0f94499d8dc28cd4909f2d5224608f1
Rakshanda1998 <gadagirakshanda98@gmail.com>
2019-12-06 17:34:59 +0530

    web module

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,1 +1,12 @@
-// Include form validation functions here
+function validation() {
+	var a = document.forms["mymovies"]["txtTitle"].value;
+	if(a == "") {
+		alert("Title is required");
+		return false;
+	}
+	if((a.length < 2) || (a.length > 65)) {
+		alert("Title should have 2 to 100 characters");
+		document.forms["mymovies"]["txtTitle"].select();
+		return false;
+	}
+	
