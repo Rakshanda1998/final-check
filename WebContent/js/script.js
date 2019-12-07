b0e3b0fc73d9f25d8d70957cffdc6ba8d00682e5
Rakshanda1998 <gadagirakshanda98@gmail.com>
2019-12-07 15:42:44 +0530

    web module2

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,12 +1,36 @@
 function validation() {
-	var a = document.forms["mymovies"]["txtTitle"].value;
+	var a = document.forms["mymovie"]["txtTitle"].value;
 	if(a == "") {
 		alert("Title is required");
 		return false;
 	}
 	if((a.length < 2) || (a.length > 65)) {
 		alert("Title should have 2 to 100 characters");
-		document.forms["mymovies"]["txtTitle"].select();
+		document.forms["mymovie"]["txtTitle"].select();
+		return false;
+	}
+	var b = document.forms["mymovie"]["txtGross"].value;
+	if(b == "") {
+		alert("Gross is Required");
+		document.forms["mymovie"]["txtGross"].focus();
+		return false;
+	}
+	if(/[^0-9]/.test(b)) {
+		alert("Gross has to be a number");
 		return false;
 	}
 	
+	var c = document.forms["mymovie"]["txtLaunch"].value;
+	if(c == "") {
+		alert("Date of Launch is required");
+		document.forms["mymovie"]["txtLaunch"].focus();
+		return false;
+	}
+	
+	var d = document.forms["mymovie"]["genre"].vlaue;
+	if(d == "") {
+		alert("Select one genre");
+		return false;
+	}
+	alert("Details submitted successfully");
+}
